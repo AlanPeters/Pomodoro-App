e2ee41019ad83eb4a1cf184512e3289e180d124d
Alan Peters <alan3051@gmail.com>
2017-12-12 20:30:34 -0700

    Added add task field

---
+++ imports/ui/TaskForm.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'; 
+
+export default class TaskForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+
+    render(){
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    type='text'
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                />
+            </form>
+        );
+    }
+
+    handleSubmit(event){
+        this.props.addTask(this.state.name);
+        event.preventDefault();
+    }
+
+    handleChange(event){
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value,
+        });
+    }
+}
--- imports/ui/TaskList.jsx
+++ imports/ui/TaskList.jsx
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 
 import Task from './Task.jsx';
+import TaskForm from './TaskForm.jsx';
 
 export default class TaskList extends Component {
     constructor(props) {
         super(props);
 
 
-
+        this.state = {
+            tasks: this.props.tasks,
+        };
+        this.addTask = this.addTask.bind(this);
     }
 
     render() {
@@ -15,21 +19,33 @@ export default class TaskList extends Component {
             return <Task taskName={item} key={index} />
         });
         return (
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Task Name</th>
-                        <th colSpan={2} />
-                        <th >Pomodoro Number</th>
-                        <th />
-                    </tr>
-                    { list }
-                </tbody>
-            </table>
+            <div>
+                <TaskForm addTask={this.addTask} />
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Task Name</th>
+                            <th colSpan={2} />
+                            <th >Pomodoro Number</th>
+                            <th />
+                        </tr>
+                        { list }
+                    </tbody>
+                </table>
+            </div>
         );
     }
 
     getTasks(){
-        return this.props.tasks;
+        return this.state.tasks;
     }
+    addTask(task){
+        let taskList = this.state.tasks.slice();
+        taskList.push(task);
+
+        this.setState({
+            tasks: taskList,
+        });
+    }
+
 }
