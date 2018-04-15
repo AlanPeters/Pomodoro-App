142ad670cef442ecdde86118a884305983831950
Alan Peters <alan3051@gmail.com>
2018-04-14 21:06:15 -0600

    Adding ability to check off tasks

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,30 +1,71 @@
 import React, {Component} from 'react';
 import Timer from '../Timer.js';
+import {Button} from 'react-bootstrap';
 
 export default class Task extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.deleteTask = this.deleteTask.bind(this); 
+        this.deleteTask = this.deleteTask.bind(this);
     }
 
-    deleteTask(){
+    deleteTask() {
         this.props.task.finish();
     }
 
-    render(){
+    render() {
         const displayTime = Timer.convertMsToHoursMinsSecs(this.props.task.getTimeSpent());
-        const minutes = displayTime.minutes.toString().padStart(2,"0")
-        const seconds = displayTime.seconds.toString().padStart(2,"0");
-        const hours = displayTime.hours.toString()+":";
-        return (
-            <tr>
-                <td>{this.props.task.getDescription()}</td>
-                <td>
-                    <button onClick={this.deleteTask}>Finish</button>
-                </td>
-                <td>{hours}{minutes}:{seconds}</td>
-                <td colSpan={3} />
-            </tr>
-        )
+        const minutes = displayTime.minutes.toString().padStart(2, "0")
+        const seconds = displayTime.seconds.toString().padStart(2, "0");
+        const hours = displayTime.hours.toString() + ":";
+        const isFinished = this.props.task.isDone();
+        const description = this.props.task.getDescription();
+        if (isFinished) {
+            return (
+                <FinishedTask
+                    hours={hours}
+                    minutes={minutes}
+                    seconds={seconds}
+                    description={description}/>
+            );
+        } else {
+            return (
+                <CurrentTask
+                    hours={hours}
+                    minutes={minutes}
+                    seconds={seconds}
+                    description={description}/>
+            );
+        }
     }
 }
+
+function FinishedTask(props){
+    return (
+        <tr>
+            <td>
+                <del>{props.description}</del>
+            </td>
+            <td>
+                <Button disabled>Finished</Button>
+            </td>
+            <td>{props.hours}{props.minutes}:{props.seconds}</td>
+            <td colSpan={3} />
+        </tr>
+    );
+}
+
+function CurrentTask(props) {
+    return (
+        <tr>
+            <td>
+                <p>{props.description}</p>
+            </td>
+            <td>
+                <Button>Finish</Button>
+            </td>
+            <td>{props.hours}{props.minutes}:{props.seconds}</td>
+            <td colSpan={3} />
+        </tr>
+    );
+}
+
