d614536771707791e1600be861f2d27fd419264b
Alan Peters <alan3051@gmail.com>
2018-08-18 13:13:48 -0600

    Simplifying synchronized timer using es6 extends

--- imports/JSObjects/SynchronizedTimer.js
+++ imports/JSObjects/SynchronizedTimer.js
@@ -1,31 +1,20 @@
 import { Meteor } from 'meteor/meteor';
 import Timer from './Timer.js';
-import { Timer as TimerState } from '../api/Timer.js';
 
-export default class SynchronizedTimer {
-  constructor(timerState) {
-    this.timer = new Timer(timerState);
-    this.setTickListener = this.timer.setTickListener.bind(this.timer);
-    this.removeTickListener = this.timer.removeTickListener.bind(this.timer);
-    this.getHoursMinutesSeconds = this.timer.getHoursMinutesSeconds.bind(this.timer);
-    this.getElapsedTimeMs = this.timer.getElapsedTimeMs.bind(this.timer);
-    this.destroy = this.timer.destroy.bind(this.timer);
-    this.getLength = this.timer.getLength.bind(this.timer);
-    this.isRunning = this.timer.isRunning.bind(this.timer);
-  }
+export default class SynchronizedTimer extends Timer {
 
-  start() {
-    this.timer.start();
+  start(){
+    super.start();
     Meteor.call('timer.insert', this.timer.toJSON());
   }
 
-  stop() {
-    this.timer.stop();
+  stop(){
+    super.stop();
     Meteor.call('timer.insert', this.timer.toJSON());
   }
 
   finish() {
-    this.timer.stop();
+    super.stop();
     Meteor.call('timer.remove');
   }
 }
